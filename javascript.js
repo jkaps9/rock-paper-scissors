31c69bc27ea213db6d2fa4a1fda6da4ecff2366d
Josh <15219491+jkaps9@users.noreply.github.com>
2025-04-27 23:05:48 -0400

    Add playGame function
    Plays 5 rounds of the game
    Moves playRound inside playGame and loops it 5 times

--- javascript.js
+++ javascript.js
@@ -27,56 +27,65 @@ function getHumanChoice(){
 
 //console.log(getHumanChoice())
 
-//initialize the score variables
-let humanScore = 0
-let computerScore = 0
+function playGame(){
+    //initialize the score variables
+    let humanScore = 0
+    let computerScore = 0
 
-function playRound(humanChoice, computerChoice){
-    //takes the human and computer player choices as arguments, plays a single round, increments the round winner’s score and logs a winner announcement.
+    function playRound(humanChoice, computerChoice){
+        //takes the human and computer player choices as arguments, plays a single round, increments the round winner’s score and logs a winner announcement.
 
-    //Make your function’s humanChoice parameter case-insensitive so that players can input “rock”, “ROCK”, “RocK”, or other variations.
-    humanChoice = humanChoice.toLowerCase()
-    let humanWins = true
-    //Write the code for your playRound function to console.log a string value representing the round winner, such as: “You lose! Paper beats Rock”.
-    switch(humanChoice) {
-        case "rock":
-            if(computerChoice=="scissors"){
-                humanWins = true
-            }else {
-                humanWins = false
-            }
-            break;
-        case "paper":
-            if(computerChoice=="rock"){
-                humanWins = true
-            }else {
-                humanWins = false
-            }
-            break;
-        case "scissors":
-            if(computerChoice=="paper"){
-                humanWins = true
-            }else {
-                humanWins = false
-            }
-            break;
-        default:
-            console.log("you made an invalid choice")
+        //Make your function’s humanChoice parameter case-insensitive so that players can input “rock”, “ROCK”, “RocK”, or other variations.
+        humanChoice = humanChoice.toLowerCase()
+        let humanWins = true
+        //Write the code for your playRound function to console.log a string value representing the round winner, such as: “You lose! Paper beats Rock”.
+        //Increment the humanScore or computerScore variable based on the round winner.
+        switch(humanChoice) {
+            case "rock":
+                if(computerChoice=="scissors"){
+                    humanWins = true
+                }else {
+                    humanWins = false
+                }
+                break;
+            case "paper":
+                if(computerChoice=="rock"){
+                    humanWins = true
+                }else {
+                    humanWins = false
+                }
+                break;
+            case "scissors":
+                if(computerChoice=="paper"){
+                    humanWins = true
+                }else {
+                    humanWins = false
+                }
+                break;
+            default:
+                console.log("you made an invalid choice")
+        }
+
+        if (humanWins){
+            humanScore++
+            console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
+        }else {
+            computerScore++
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
+        }
+
+        
+        console.log(`Human: ${humanScore} Computer: ${computerScore}`)
     }
 
-    if (humanWins){
-        humanScore++
-        console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
-    }else {
-        computerScore++
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
-    }
+    
 
-    //Increment the humanScore or computerScore variable based on the round winner.
+    for (i=0;i<5;i++){
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    }
 
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+playGame()
