38608c04d84dc60eff67939462451165e3b62212
Josh <15219491+jkaps9@users.noreply.github.com>
2025-05-01 17:07:25 -0400

    Refactor code to use GUI instead of console
    - Add buttons for user choice with onclick functionality to play the game
    - Remove unneeded code from script

--- javascript.js
+++ javascript.js
@@ -1,91 +1,79 @@
-//console.log("Hello World")
-
-function getComputerChoice(){
+function getComputerChoice() {
     // randomly returns one of the following string values: “rock”, “paper” or “scissors”
 
     //create array with the 3 choices
     const choices = ["rock", "paper", "scissors"]
-    
+
     // Use Math.Random to generate random number betweeen 0 and 2
-    let index = Math.floor(Math.random()*3)
-    
+    let index = Math.floor(Math.random() * 3)
+
     // Use random number to determine the output
     return choices[index]
 }
 
-//console.log(getComputerChoice())
+function playRound(humanChoice, computerChoice) {
+    //takes the human and computer player choices as arguments, plays a single round, increments the round winner’s score and logs a winner announcement.
 
-function getHumanChoice(){
-
-    //return one of the valid choices depending on what the user inputs.
-    
-    let humanChoice = prompt("Enter your choice", '')
-
-    //for now assuming user always inputs valid choice, so just returning the input
-    return humanChoice
-}
-
-//console.log(getHumanChoice())
-
-function playGame(){
-    //initialize the score variables
-    let humanScore = 0
-    let computerScore = 0
-
-    function playRound(humanChoice, computerChoice){
-        //takes the human and computer player choices as arguments, plays a single round, increments the round winner’s score and logs a winner announcement.
-
-        //Make your function’s humanChoice parameter case-insensitive so that players can input “rock”, “ROCK”, “RocK”, or other variations.
-        humanChoice = humanChoice.toLowerCase()
-        let humanWins = true
-        //Write the code for your playRound function to console.log a string value representing the round winner, such as: “You lose! Paper beats Rock”.
-        //Increment the humanScore or computerScore variable based on the round winner.
-        switch(humanChoice) {
-            case "rock":
-                if(computerChoice=="scissors"){
-                    humanWins = true
-                }else {
-                    humanWins = false
-                }
-                break;
-            case "paper":
-                if(computerChoice=="rock"){
-                    humanWins = true
-                }else {
-                    humanWins = false
-                }
-                break;
-            case "scissors":
-                if(computerChoice=="paper"){
-                    humanWins = true
-                }else {
-                    humanWins = false
-                }
-                break;
-            default:
-                console.log("you made an invalid choice")
-        }
-
-        if (humanWins){
-            humanScore++
-            console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
-        }else {
-            computerScore++
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
-        }
-
-        
-        console.log(`Human: ${humanScore} Computer: ${computerScore}`)
+    //Make your function’s humanChoice parameter case-insensitive so that players can input “rock”, “ROCK”, “RocK”, or other variations.
+    humanChoice = humanChoice.toLowerCase()
+    let humanWins = true
+    //Write the code for your playRound function to console.log a string value representing the round winner, such as: “You lose! Paper beats Rock”.
+    //Increment the humanScore or computerScore variable based on the round winner.
+    switch (humanChoice) {
+        case "rock":
+            if (computerChoice == "scissors") {
+                humanWins = true
+            } else {
+                humanWins = false
+            }
+            break;
+        case "paper":
+            if (computerChoice == "rock") {
+                humanWins = true
+            } else {
+                humanWins = false
+            }
+        case "scissors":
+            if (computerChoice == "paper") {
+                humanWins = true
+            } else {
+                humanWins = false
+            }
+            break;
+        default:
+            console.log("you made an invalid choice")
     }
 
-    
+    if (humanWins) {
+        humanScore++
+        console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
+    } else {
+        computerScore++
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
+    }
 
-    for (i=0;i<5;i++){
-        const humanSelection = getHumanChoice();
+
+    console.log(`Human: ${humanScore} Computer: ${computerScore}`)
+}
+
+
+//initialize the score variables
+let humanScore = 0
+let computerScore = 0
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll("button");
+console.log(buttons)
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+    // and for each one we add a 'click' listener
+    button.addEventListener("click", () => {
         const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
 
-}
+        //alert(button.id);
+        if (button.id === "rock") { playRound("rock", computerSelection) }
+        if (button.id === "paper") { playRound("paper", computerSelection) }
+        if (button.id === "scissors") { playRound("scissors", computerSelection) }
+    });
+});
 
-playGame()
